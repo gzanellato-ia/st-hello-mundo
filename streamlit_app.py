05fa28e7542b85dc4be6a55f2956b95f4b0fa06f
gzanellato-ia <54750170+gzanellato-ia@users.noreply.github.com>
2024-04-10 15:38:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.write('Hello mundo!')
 
 import pandas as pd
-from io import StringIO
+#from io import StringIO
 import matplotlib.pyplot as plt
 
 dfPlot = pd.read_csv('20240402_022821_YPF.Nq.LACh-465(h).csv', skiprows=[1])
