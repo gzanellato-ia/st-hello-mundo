b790275e15ed64a45ae4ac668be243b79350b7a8
gzanellato-ia <54750170+gzanellato-ia@users.noreply.github.com>
2024-04-10 16:24:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pandas as pd
 #from io import StringIO
 import matplotlib.pyplot as plt
 
-dfPlot = pd.read_csv('20240402_022821_YPF.Nq.LACh-465(h).csv', skiprows=[1])
+df1 = pd.read_csv('20240402_022821_YPF.Nq.LACh-465(h).csv', skiprows=[1])
 st.write(dfPlot.head(4))
 
 DepthIn = 2624
