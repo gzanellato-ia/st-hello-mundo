11924e76871403f06a254897654e725d417cd868
gzanellato-ia <54750170+gzanellato-ia@users.noreply.github.com>
2024-04-10 15:28:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.write('Hello mundo!')
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader('20240402_022821_YPF.Nq.LACh-465(h).csv')
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
+    dataframe = pd.read_csv(uploaded_file, skiprows=[1])
     st.write(dataframe)
