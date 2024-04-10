6ae214cda606fcac5154479d8e59462d6b130600
gzanellato-ia <54750170+gzanellato-ia@users.noreply.github.com>
2024-04-10 15:30:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,11 @@ st.write('Hello mundo!')
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader('20240402_022821_YPF.Nq.LACh-465(h).csv')
-if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file, skiprows=[1])
-    st.write(dataframe)
+dataframe = pd.read_csv('20240402_022821_YPF.Nq.LACh-465(h).csv', skiprows=[1])
+st.write(dataframe)
+
+#uploaded_file = st.file_uploader('20240402_022821_YPF.Nq.LACh-465(h).csv')
+#if uploaded_file is not None:
+#    # Can be used wherever a "file-like" object is accepted:
+#    dataframe = pd.read_csv(uploaded_file, skiprows=[1])
+#    st.write(dataframe)
